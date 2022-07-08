a1ac04478924e9fe801595f7023392f0570b28fc
Chaitanya <67656388+ChhikaraBRUH@users.noreply.github.com>
2022-07-09 05:08:15 +0530

    chore: setting up redux

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -6,13 +6,30 @@ import { formatDate } from "../utils/authUtils";
  * */
 
 export const users = [
-  {
-    _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    username: "adarshbalika",
-    password: "adarshBalika123",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-  },
+	{
+		_id: uuid(),
+		firstName: "Adarsh",
+		lastName: "Balika",
+		username: "adarshbalika@gmail.com",
+		userHandler: "adarshbalika",
+		password: "password",
+		bio: "This is a sample bio.",
+		link: "https://adarshbalika.netlify.app",
+		profilePic: "https://pbs.twimg.com/profile_images/1525696264364507136/HsQgVOfP_400x400.jpg",
+		createdAt: formatDate(),
+		updatedAt: formatDate(),
+	},
+	{
+		_id: uuid(),
+		firstName: "Chaitanya",
+		lastName: "Chhikara",
+		username: "chaitanya@gmail.com",
+		userHandler: "chaitanya",
+		password: "password",
+		bio: "Learning web dev right now.",
+		link: "https://bruh.dev",
+		profilePic: "https://pbs.twimg.com/profile_images/1494970975942688770/mVmFT4P__400x400.jpg",
+		createdAt: formatDate(),
+		updatedAt: formatDate(),
+	},
 ];
